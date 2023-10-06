ba6680e62fbbf3aa41457b47b351e89ad3d8260b
marcoscarvalho23 <marcoscarvalhop@outlook.com>
2023-10-05 22:28:37 -0300

    ForeignKey

---
+++ Migrations/20231006012201_DepartmentForeignKey.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebMVC.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Seller",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "SalesRecord",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Seller",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "SalesRecord",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -16,4 +16,11 @@ public class SellerService
     {
         return _context.Seller.ToList();
     }
+
+    public void Insert(Seller obj)
+    {
+        obj.Department = _context.Department.First();
+        _context.Add(obj);
+        _context.SaveChanges();
+    }
 }
