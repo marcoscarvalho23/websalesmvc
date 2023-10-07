5a164f5415a39cf2e668a15264a495910db0448d
marcoscarvalho23 <marcoscarvalhop@outlook.com>
2023-10-07 20:00:42 -0300

    Edit Seller

---
+++ Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace websalesmvc.Services.Exceptions;
+public class DbConcurrencyException : ApplicationException
+{
+    public DbConcurrencyException(string message) : base (message)
+    {
+    }
+}
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using websalesmvc.Services.Exceptions;
 
 namespace SalesWebMVC.Services;
 
@@ -42,5 +43,23 @@ public class SellerService
         var obj = _context.Seller.Find(id);
         _context.Seller.Remove(obj);
         _context.SaveChanges();
+    }
+
+    public void Update(Seller obj)
+    {
+        if (!_context.Seller.Any(x => x.Id == obj.Id))
+        {
+             throw new NotFoundException("Seller not found! ");
+        }
+        try
+        {
+            _context.Update(obj);
+            _context.SaveChanges();
+        }
+        catch (DbConcurrencyException e)
+        {
+            
+            throw new DbConcurrencyException(e.Message);
+        } 
     }
 }
